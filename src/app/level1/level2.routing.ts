6ef8924b637c69ec6ea8717ee47b16f44f6ee96f
nekkon <nekkon01@gmail.com>
2017-05-15 14:56:57 +0300

    various changes

--- src/app/level1/level2.routing.ts
+++ src/app/level1/level2.routing.ts
@@ -1,9 +1,47 @@
 import { Routes }    from '@angular/router';
 
 export const routes: Routes = [
-  { path: '',   redirectTo: '/view1', pathMatch: 'full' },
-  { path: 'view1', loadChildren: 'app/level1/view1/view1.module#view1Module' },
-  { path: 'view2', loadChildren: 'app/level1/view2/view2.module#view2Module' },
-  { path: 'level3', loadChildren: 'app/level1/level2/level3.module#level3Module' },
-  { path: '**', loadChildren: 'app/level1/view2/view2.module#view2Module' }
+  { path: '', redirectTo: '/view1', pathMatch: 'full' },
+    { path:'view1', component: LoginPage },
+    { path:'level3', component: level3Component, 
+        canActivateChild: [CanActivateRoute],
+        children: [
+            {
+                path: '',
+                component: IncomingPage,
+                outlet: 'view'
+            },
+            {
+                path: 'incoming',
+                component: IncomingPage,
+                outlet: 'view'
+            },
+            {
+                path: 'outstanding',
+                component: OutstandingPage,
+                outlet: 'view'
+            },
+            {
+                path: 'draft',
+                component: DraftPage,
+                outlet: 'view'
+            },
+            {
+                path: 'completed',
+                component: CompletedPage,
+                outlet: 'view'
+            },
+            {
+                path: 'new',
+                component: NewDocPage,
+                outlet: 'view'
+            },
+            { 
+                path:'**', 
+                component: Four04Page, 
+                outlet: 'view' 
+            }
+        ]
+    },
+
 ];
